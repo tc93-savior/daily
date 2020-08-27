58794d20ccfcc88199dc2981ef7b42b035989b05
jt <1060257099@qq.com>
2020-08-27 11:53:33 +0800

    daily

--- src/components/collection/puzzle.vue
+++ src/components/collection/puzzle.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>css 拼图特效收集</div>
+  <div>
+    {{ a() }}<button @click="change">test</button
+    ><button @click="changeRevert">test2</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'puzzle'
-}
+  name: 'puzzle',
+  data() {
+    return {
+      type: 1
+    };
+  },
+  computed: {
+    a: function () {
+      let tmp;
+      if (this.type === 1) {
+        tmp = () => {
+          return 2222;
+        };
+      }
+
+      if (this.type === 2) {
+        tmp = () => {
+          return 2343;
+        };
+      }
+
+      return tmp;
+    }
+  },
+  methods: {
+    change() {
+      this.type = 2;
+    },
+    changeRevert() {
+      this.type = 1;
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
