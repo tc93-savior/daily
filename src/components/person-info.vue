c709f5f783a54749276366280c56e183a582f799
jt <1060257099@qq.com>
2020-12-04 15:23:07 +0800

    daily

--- src/components/person-info.vue
+++ src/components/person-info.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="person-info">
-    <!--有空得把简历搬到这个页面上来-->
+    <div class="item1">1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+    <div class="item5">5</div>
   </div>
 </template>
 
@@ -8,14 +12,28 @@
 export default {
   name: 'personInfo',
   components: {},
-  data () {
-    return {}
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <style>
-  .person-info {
-
-  }
+.person-info {
+  border: 1px solid black;
+  display: grid;
+  grid-template-columns: 100px 200px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-template-areas:
+    "a b"
+    "a c"
+    "a d"
+    "e e";
+}
+.item1 {
+  grid-area: a;
+}
+.item5{
+  grid-area: e;
+}
 </style>
